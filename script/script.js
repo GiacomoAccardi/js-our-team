7aff2808e6246c7f3b2bfad5779d4c7bb158794d
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-10 15:59:11 +0200

    [UPD] milestone 0 DONE

--- script/script.js
+++ script/script.js
@@ -12,11 +12,36 @@ BONUS-----------------------------------
 
 //creo un array in cui inserire le info di ogni membro del team 
 const members = [
-    'Wayne Barnett', 
-    'Angela Caroll',
-    'Walter Gordon',
-    'Angela Lopez',
-    'Scott Estrada',
-    'Barbara Ramos'
+
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'
+    }, 
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg'
+    }, 
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg'
+    }, 
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+    }, 
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg'
+    }
+    
 ]
-console.log(members)
