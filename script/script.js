535e606b992a90b871fd3c4870d7754007fb3d91
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-10 15:45:39 +0200

    [upd] there are now some instruction for the base exercize into js file

---
+++ script/script.js
@@ -0,0 +1,11 @@
+/* 
+
+1.Creare un array contenete oggetti
+  >>gli oggetti conterranno le informazioni di ogni membro del team
+2. Mostrare in console tutte le informazioni per ogni membro 
+3. inniettare le informazioni nel DOM e quindi farle comparire a schermo 
+
+BONUS-----------------------------------
+
+
+*/
